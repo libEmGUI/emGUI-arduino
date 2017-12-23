1b9158c4079108e5cd1761e5190c6476ae502a21
Roman Savrulin <savrulin@flexibity.com>
2017-12-24 01:46:34 +0300

    Fix header guards

--- src/emGUI.h
+++ src/emGUI.h
@@ -1,5 +1,10 @@
+#ifndef EMGUI_ARDUINO_H
+#define EMGUI_ARDUINO_H
+
 #ifdef __cplusplus
 #include <Adafruit_GFX.h> //to add GFX to arduino's include path
 #endif
 
 #include "emGUI/emGUI.h"
+
+#endif
