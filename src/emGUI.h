c50d26657270274d829bc56f2ef0697f677fc648
Roman Savrulin <savrulin@flexibity.com>
2018-01-11 09:53:03 +0300

    Add macro for PROGMEM strings

--- src/emGUI.h
+++ src/emGUI.h
@@ -5,6 +5,9 @@
 #include <Adafruit_GFX.h> //to add GFX to arduino's include path
 #endif
 
+//Wrapper for passing PROGMEM placed strings to const char* methods of emGUI
+#define F_WRP(const_char_p) (String(F(const_char_p)).c_str()) 
+
 #include "emGUI/emGUI.h"
 
 #endif
