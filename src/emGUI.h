2dd7b5e5bd01acd4ac70b723a0ca713e8ddb55cc
Roman Savrulin <savrulin@flexibity.com>
2018-02-02 20:31:33 +0300

    add macro stubs for MSVC library

--- src/emGUI.h
+++ src/emGUI.h
@@ -1,12 +1,14 @@
 #ifndef EMGUI_ARDUINO_H
 #define EMGUI_ARDUINO_H
 
-#ifdef __cplusplus
+#if defined(__cplusplus) && !defined(_MSC_VER)
 #include <Adafruit_GFX.h> //to add GFX to arduino's include path
 #endif
 
+#if !defined(_MSC_VER)
 //Wrapper for passing PROGMEM placed strings to const char* methods of emGUI
 #define F_WRP(const_char_p) (String(F(const_char_p)).c_str()) 
+#endif
 
 #include "emGUI/emGUI.hpp"
 
