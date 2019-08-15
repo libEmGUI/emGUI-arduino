eb6a157b8874bd21c73ca2e3d7ae377af8980e3a
Roman Savrulin <savrulin@flexibity.com>
2019-08-15 21:37:46 +0300

    update lib

--- src/TouchWrapper.cpp
+++ src/TouchWrapper.cpp
@@ -7,27 +7,6 @@ TouchWrapper::TouchWrapper(Adafruit_GFX &tft): _tft(tft){
 	lastTouched = false;
 }
 
-TouchWrapper::TouchPoint TouchWrapper::TouchPoint::transform(float * matrix, size_t size){
-	TouchPoint tp;
-
-	size_t matrixSize = 3;
-	if (size != matrixSize*matrixSize)
-		return tp;
-
-	float point[] = {(float)x, (float)y, 1.f};
-	float transformed[] = {0.0f, 0.0f, 0.0f};
-
-	for(size_t i = 0; i < matrixSize; i++){
-		for(size_t j = 0; j < matrixSize; j++){
-			transformed[i] += point[j] * matrix [i*matrixSize + j]; 
-		}
-	}
-
-	tp.x = (int) transformed[0];
-	tp.y = (int) transformed[1];
-	return tp;
-}
-
 bool TouchWrapper::loop()
 {
 	currentTouch.eventTime = millis();
@@ -39,14 +18,16 @@ bool TouchWrapper::loop()
 		
 		auto p = getTouchCoords();
 
-		if (p.x < 0) p.x = 0;
-		if (p.x > _tft.width()) p.x = _tft.width();
+		auto pt = TouchPoint(p.x, p.y).transform(_transformMatrix);
 
-		if (p.y < 0) p.y = 0;
-		if (p.y > _tft.height()) p.y = _tft.height();
+		if (pt.x < 0) pt.x = 0;
+		if (pt.x > _tft.width()) pt.x = _tft.width();
 
-		currentTouch.x = p.x;
-		currentTouch.y = p.y;
+		if (pt.y < 0) p.y = 0;
+		if (pt.y > _tft.height()) pt.y = _tft.height();
+
+		currentTouch.x = pt.x;
+		currentTouch.y = pt.y;
 
 		if (!lastTouched)
 			currentTouch.event = pushTs;
--- src/TouchWrapper.h
+++ src/TouchWrapper.h
@@ -13,7 +13,24 @@ public:
 		TouchPoint(int newX, int newY): x(newX), y(newY){}; 
 		int x;
 		int y;
-		TouchPoint transform(float * matrix, size_t size);
+
+		template<typename T, size_t N> 
+		TouchPoint transform(T (&matrix)[N]){
+			size_t matrixSize = 3;
+			if (N != matrixSize*matrixSize)
+				return TouchPoint();
+
+			T point[] = {(T)x, (T)y, (T)1};
+			T transformed[] = {(T)0, (T)0, (T)0};
+
+			for(size_t i = 0; i < matrixSize; i++){
+				for(size_t j = 0; j < matrixSize; j++){
+					transformed[i] += point[j] * matrix [i*matrixSize + j]; 
+				}
+			}
+
+			return TouchPoint(transformed[0], transformed[1]);
+		}
 	};
 
 	TouchWrapper(Adafruit_GFX &tft);
@@ -22,16 +39,53 @@ public:
 	virtual bool isTouched() = 0;
 	virtual TouchPoint getTouchCoords() = 0;
 
+	// Applies _transformMatrix DOT matrix
+	template<size_t N>
+	void dotTransformation(float (&matrix)[N]) {
+		size_t matrixSize = 3;
+		if (N != matrixSize*matrixSize)
+			return;
+
+		float _tmpMatrix[matrixSize][matrixSize];
+
+		memcpy(_tmpMatrix, _transformMatrix, sizeof(_tmpMatrix));
+		
+		for(size_t i = 0; i < matrixSize; i++){
+			for(size_t j = 0; j < matrixSize; j++){
+				float tmp = 0.f;
+				for(size_t k = 0; k < matrixSize; k++){
+					tmp += _transformMatrix[k + matrixSize*i] * matrix[k*matrixSize + j];
+				}
+				_tmpMatrix[i][j] = tmp;
+			}
+		}
+
+		//apply
+		memcpy(_transformMatrix, _tmpMatrix, sizeof(_transformMatrix));
+	}
+
+	void dumpMatrix(){
+		size_t matrixSize = 3;
+		Serial.printf("Transformation Matrix is: \n");
+		for(size_t i = 0; i < matrixSize; i++){
+			for(size_t j = 0; j < matrixSize; j++){
+				Serial.printf("%f, ", _transformMatrix[i*matrixSize + j]);
+			}
+			Serial.printf("\n");
+		}
+	}
+
 protected:
 	volatile xTouchEvent currentTouch;
 	volatile bool bTouchInt = false;
 	bool alreadyPoped = true;
 	bool lastTouched = false;
-	float xScale = 1.0f;
-	float yScale = 1.0f;
-	int xOffset = 0;
-	int yOffset = 0;
-	int xMax = 100;
-	int yMax = 100;
+
+	// I matrix first
+	float _transformMatrix[9] = {
+		1.0f, 0.f, 0.f,
+		0.f, 1.0f, 0.f,
+		0.f, 0.f, 1.f
+	};
 	Adafruit_GFX &_tft;
 };
